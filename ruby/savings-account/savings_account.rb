d42403c7dbe52d8f32bd17f4f4fee546d01d0fca
s-sakashi <suguru.sakashita@gmail.com>
2021-11-08 17:19:53 +0900

    [add]'saving_account'完成

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,26 @@
 module SavingsAccount
+  
+  INTEREST_RATE = [
+    {balance: -Float::INFINITY...0 , rate: -3.213},
+    {balance: 0...1000, rate: 0.5},
+    {balance: 1000...5000, rate: 1.621}, 
+    {balance: 5000..Float::INFINITY , rate: 2.475}
+  ]
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    INTEREST_RATE.find{|ir| ir[:balance].include?(balance)}[:rate]
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance + balance.abs * interest_rate(balance) / 100
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    balance_of_the_year = current_balance
+    year = 0
+    while balance_of_the_year < desired_balance
+      balance_of_the_year = annual_balance_update(balance_of_the_year)
+      year += 1
+    end
+    year
   end
 end
