3a79c3d2f9ccc5ea2ff643c4faedf06a4ecfb0b2
suguru_sakashita <suguru@sugurusitanombp.lan>
2021-11-08 13:56:35 +0900

    [add]'assembly_line'完成

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
 class AssemblyLine
+  
   def initialize(speed)
+    @production_volume = 221.0
+    @success_rate = {'safety': 1.0, 'nomal': 0.9, 'low': 0.8, 'very_low': 0.77}
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    if @speed <= 4
+      success_ratio = :safety
+    elsif @speed <= 8
+      success_ratio = :nomal
+    elsif @speed <= 9
+      success_ratio = :low
+    else
+      success_ratio = :very_low
+    end
+    (@production_volume * @speed * @success_rate[success_ratio]).round(1)
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
+
+AssemblyLine.new(1).production_rate_per_hour
