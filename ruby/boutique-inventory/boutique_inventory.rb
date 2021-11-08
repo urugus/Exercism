9952a9ad85cb73528e2eb65f558083e1bb6f2f4e
s-sakashi <suguru.sakashita@gmail.com>
2021-11-08 18:59:53 +0900

    [add]'bountique_inventory_test'の完成

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -1,26 +1,29 @@
 class BoutiqueInventory
+
+  COST_STANDARD = 30
+
   def initialize(items)
     @items = items
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map{|item|  item[:name]}.sort!
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select{|item| item[:price] < COST_STANDARD}
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select{|item| item[:quantity_by_size].empty?}
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    @items.find{|item| item[:name] == name}[:quantity_by_size]
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+    @items.map{ |item| item[:quantity_by_size].values.sum}.sum
   end
 
   private
