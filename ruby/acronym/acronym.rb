db15da248a1fb1883e0d6055fde486f894282325
s-sakashi <suguru.sakashita@gmail.com>
2021-11-09 13:16:54 +0900

    [add]'acronym'のレビュー後の修正

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,8 +1,7 @@
 class Acronym
   class << self
     def abbreviate(words)
-      words_list = words.scan(/[A-z]+|[A-z]+-/)
-      words_list.map{|w| w[0]}.join().upcase
+      words_list = words.scan(/\b\w/).join.upcase
     end
   end
 end
