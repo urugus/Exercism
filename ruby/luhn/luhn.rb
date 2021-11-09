764873888795ac65f2804f0c9f4f8f5e696cc4fa
s-sakashi <suguru.sakashita@gmail.com>
2021-11-09 18:36:11 +0900

    [add]'luhn'の回答途中

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,7 +1,23 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+class Luhn
+  class << self
+    def valid?(input)
+      digits_list = input.split
+      return false unless digits_list.all?{|d| d.match?(/^\d+\d+$/)}
+      digits_list.map{|d| doubling_every_second_digit(d)}.sum % 10 == 0
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
+    # 偶数番目の数字を２倍（９を超える場合は９を引く）して合計値を返す
+    def doubling_every_second_digit(digits_string)
+      sum = 0
+      puts digits_string
+      digits_string.chars.each_with_index do |d, idx| 
+        puts idx.even? ? (d.to_i * 2).to_s.chars.map(&:to_i).sum: d.to_i
+      end
+      puts sum
+      sum
+    end
+
+  end
+end
+
+Luhn.valid?("000 0")
