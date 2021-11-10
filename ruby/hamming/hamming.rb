73786a83426697a9b44a6c664d403eddc98f5d2b
s-sakashi <suguru.sakashita@gmail.com>
2021-11-10 18:14:58 +0900

    [add]'hamming'のレビュー後修正

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,8 @@
 class Hamming
   class << self
     def compute(dna1, dna2)
-      # binding.irb
       raise ArgumentError if dna1.length != dna2.length
-      dna1.chars.each_with_index.inject(0) {|mistakes, (d1, idx)|
-        d1 != dna2[idx] ? mistakes += 1 : mistakes
-      }
+      dna1.chars.zip(dna2.chars).count{|d1, d2| d1 != d2}
     end
   end
 end
-
-puts Hamming.compute('GGACTGAAATCTG', 'AAACTGAAATCTG')
