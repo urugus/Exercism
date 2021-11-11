c186b91222e1b251b3f2e6590650208985b3dd8f
s-sakashi <suguru.sakashita@gmail.com>
2021-11-11 14:27:07 +0900

    [add]'Space Age'の完成

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,7 +1,27 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+class SpaceAge
+  
+  SECONDS_IN_1YEAR_ON_EARTH = 60 * 60 * 24 * 365.25
+  
+  SPACE_AGES = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+  
+  
+  def initialize(second)
+    @year_on_earth = second / SECONDS_IN_1YEAR_ON_EARTH
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+  SPACE_AGES.each {|key, value|
+    define_method "on_#{key}" do
+      (@year_on_earth / value).round(2)
+    end
+  }
+  
+end
