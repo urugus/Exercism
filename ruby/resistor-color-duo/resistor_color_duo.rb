6278e5294a5a28e97f3fd7fb780e8c33df37ead3
s-sakashi <suguru.sakashita@gmail.com>
2021-11-12 14:07:38 +0900

    [add]'Resistor Color Duo'の完成

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,20 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
+class ResistorColorDuo
+  COLOR_CODED_BANDS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
+  class << self
+    def value(colors)
+      colors[0..1].map(&:to_sym).each.inject([]){|array, sym| array << COLOR_CODED_BANDS[sym] }.join.to_i
+    end
+  end
+end
