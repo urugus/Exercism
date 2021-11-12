c2493ac462195da33600130441e8cea0720b2af0
s-sakashi <suguru.sakashita@gmail.com>
2021-11-12 13:09:02 +0900

    [add]'beer-song'の完成

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,39 +1,36 @@
 class BeerSong
+  
+  MAX_STOCK = 99
+  
   class << self
 
-    GO_TO_THE_STORE = "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-
-    def sing_item_on_the_wall(item)
-      "#{item.count.capitalize} of beer on the wall, #{item.count} of beer."
-    end
-
-    def sing_take_down(item)
-      "Take #{item.pronoun} down and pass it around, #{item.count} of beer on the wall.\n"
+    def sing_one_phrase(item, item_after_consume)
+      phrase = "#{item.count.capitalize} of #{item.name} on the wall, #{item.count} of #{item.name}.\n"
+      phrase += if item.quantity == 0
+            item_after_adding_stock = Item.new(name: item.name, quantity: MAX_STOCK, unit: item.unit) 
+            "Go to the store and buy some more, #{item_after_adding_stock.count} of #{item_after_adding_stock.name} on the wall.\n"
+          else
+            "Take #{item.pronoun} down and pass it around, #{item_after_consume.count} of #{item_after_consume.name} on the wall.\n"
+          end
     end
 
     def recite(quantity, loop_time)
-      bottle = Item.new(quantity: quantity, unit: 'bottle')
-      bottle_consumption = Item.new(quantity: 1, unit: 'bottle')
+      beer = Item.new(name: 'beer', quantity: quantity, unit: 'bottle')
+      beer_consumption = Item.new(name: 'beer', quantity: 1, unit: 'bottle')
 
-      song = loop_time.times.inject('') { |song, c|
-        song += "#{sing_item_on_the_wall(bottle)}\n"
-        if bottle.quantity == 0
-          song += GO_TO_THE_STORE
-        else
-          bottle = (bottle - bottle_consumption)
-          song += sing_take_down(bottle)
-        end
-      }
+      phrases = loop_time.times.inject([]) { |phrases, c|
+        phrases << sing_one_phrase(beer, beer = beer - beer_consumption)
+      }.join("\n")
     end
   end
 end
 
 
 class Item
-  attr_reader :quantity, :unit
+  attr_reader :name, :quantity, :unit
 
-  def initialize(quantity: 0, unit: '')
-    raise ArgumentError if quantity < 0
+  def initialize(name: '', quantity: 0, unit: '')
+    @name = name
     @quantity = quantity
     @unit = unit
   end
@@ -54,8 +51,10 @@ class Item
   end
 
   def -(other)
-    raise ArgumentError unless @unit == other.unit
-    Item.new(quantity: @quantity - other.quantity, unit: @unit)
+    raise ArgumentError unless @unit == other.unit && @name == other.name
+    Item.new(name: @name, quantity: @quantity - other.quantity, unit: @unit)
   end
 
 end
+
+puts BeerSong.recite(99, 2)
