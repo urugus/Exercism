432fb09617e777b0f6b987ccb9dd4c20a257b568
s-sakashi <suguru.sakashita@gmail.com>
2021-11-12 11:17:09 +0900

    [add]'beer-song'の途中経過保存

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,7 +1,61 @@
-=begin
-Write your code for the 'Beer Song' exercise in this file. Make the tests in
-`beer_song_test.rb` pass.
+class BeerSong
+  class << self
 
-To get started with TDD, see the `README.md` file in your
-`ruby/beer-song` directory.
-=end
+    GO_TO_THE_STORE = "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+    def sing_item_on_the_wall(item)
+      "#{item.count.capitalize} of beer on the wall, #{item.count} of beer."
+    end
+
+    def sing_take_down(item)
+      "Take #{item.pronoun} down and pass it around, #{item.count} of beer on the wall.\n"
+    end
+
+    def recite(quantity, loop_time)
+      bottle = Item.new(quantity: quantity, unit: 'bottle')
+      bottle_consumption = Item.new(quantity: 1, unit: 'bottle')
+
+      song = loop_time.times.inject('') { |song, c|
+        song += "#{sing_item_on_the_wall(bottle)}\n"
+        if bottle.quantity == 0
+          song += GO_TO_THE_STORE
+        else
+          bottle = (bottle - bottle_consumption)
+          song += sing_take_down(bottle)
+        end
+      }
+    end
+  end
+end
+
+
+class Item
+  attr_reader :quantity, :unit
+
+  def initialize(quantity: 0, unit: '')
+    raise ArgumentError if quantity < 0
+    @quantity = quantity
+    @unit = unit
+  end
+
+  def count
+    case @quantity
+    when 0
+      "no more #{@unit}s"
+    when 1
+      "1 #{unit}"
+    else
+      "#{@quantity} #{unit}s"
+    end
+  end
+
+  def pronoun 
+    @quantity == 1 ? 'it' : 'one'
+  end
+
+  def -(other)
+    raise ArgumentError unless @unit == other.unit
+    Item.new(quantity: @quantity - other.quantity, unit: @unit)
+  end
+
+end
--- ruby/beer-song/beer_song_test.rb
+++ ruby/beer-song/beer_song_test.rb
@@ -13,7 +13,7 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_last_generic_verse
-    skip
+    # skip
     expected = <<~TEXT
       3 bottles of beer on the wall, 3 bottles of beer.
       Take one down and pass it around, 2 bottles of beer on the wall.
@@ -22,7 +22,7 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_verse_with_2_bottles
-    skip
+    # skip
     expected = <<~TEXT
       2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall.
