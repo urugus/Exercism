242bd240aedf9c6cfc66042ce68ded474f906442
s-sakashi <suguru.sakashita@gmail.com>
2021-11-12 18:31:21 +0900

    [add]'Scrabble Score'の途中経過保存

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,7 +1,28 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+class Scrabble
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+  SCORE =  [
+    {letters: %w[a e i o u l n r s], value: 1},
+    {letters: %w[d g], value: 2},
+    {letters: %w[b c m p], value: 3},
+    {letters: %w[f h v m y], value: 4},
+    {letters: %w[k], value: 5},
+    {letters: %w[j x], value: 8},
+    {letters: %w[q z], value: 10}
+  ]
+
+  def initialize(word)
+    @alpahbets = word.downcase.chars
+  end
+
+  def score
+    @alpahbets.each_with_object(0){|a, sum|
+      binding.irb
+      sum += SCORE.each_with_object(0) {|s, v|
+        v += s[:value] if s[:letters].include?(a)
+      }
+    }
+  end
+
+end
+
+p Scrabble.new('fact').score
