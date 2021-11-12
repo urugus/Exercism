e9ffd852837eeed093d9e1f3e6ba7666d42cd5b7
s-sakashi <suguru.sakashita@gmail.com>
2021-11-12 14:58:28 +0900

    [add]'High Scores'の完成

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,7 +1,25 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
+  def initialize(scores)
+    @scores = scores
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+  def scores
+    @scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.max(3).sort.reverse
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
