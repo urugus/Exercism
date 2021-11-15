31ed084e2a01a41429b15585e879443fff284fbd
s-sakashi <suguru.sakashita@gmail.com>
2021-11-15 15:29:59 +0900

    [add]'Leap'の完成

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,16 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
+# class GregorianDate　< Date
+class Year
+  class << self
+    def leap?(year)
+      if year % 4 == 0
+        if year % 100 == 0 && year % 400 != 0
+          false
+        else
+          true
+        end
+      else
+        false
+      end
+    end
+  end
+end
