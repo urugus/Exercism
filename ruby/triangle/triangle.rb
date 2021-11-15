2fa689aa338f26b1fe2265ee59a273e84436a8bd
s-sakashi <suguru.sakashita@gmail.com>
2021-11-15 18:44:14 +0900

    [add]'Triangle'の完成

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,7 +1,22 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+class Triangle
+  def initialize(edges)
+    @edges = edges
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+  def equilateral?
+    is_complited? && @edges.all?(@edges[0])
+  end
+
+  def isosceles?
+    is_complited? && @edges.sort.combination(2).uniq.size <= 2
+  end
+
+  def scalene?
+    is_complited? && !equilateral? && !isosceles?
+  end
+
+  def is_complited?
+    perimeter = @edges.sum
+    @edges.all?{|e| 2 * e  < perimeter}
+  end
+end
