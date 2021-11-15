176c72900473fbbf7ee0b16ec6d3b70215318b8f
s-sakashi <suguru.sakashita@gmail.com>
2021-11-15 13:53:14 +0900

    [add]'Gigasecond'の完成

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,7 +1,7 @@
-=begin
-Write your code for the 'Gigasecond' exercise in this file. Make the tests in
-`gigasecond_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/gigasecond` directory.
-=end
+class Gigasecond
+  class << self
+    def from(utc_time)
+      Time.at(utc_time.to_f + 10**9)
+    end
+  end
+end
