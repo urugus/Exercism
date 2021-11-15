0114c9363e6b8ad7ee603bd45cc7ddf63d68aab3
s-sakashi <suguru.sakashita@gmail.com>
2021-11-15 19:09:57 +0900

    [add]'Difference Of Squares'の完成

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,7 +1,18 @@
-=begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+class Squares
+  def initialize(num)
+    @num = num
+    @nums =  [*1..@num]
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
-=end
+  def square_of_sum
+    @nums.sum ** 2
+  end
+
+  def sum_of_squares
+    @nums.map{|n| n ** 2}.sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
