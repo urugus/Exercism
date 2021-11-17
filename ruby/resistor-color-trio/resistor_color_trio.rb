43dc7d0ec6d76aa3ebb9918c92aac2d628185769
s-sakashi <suguru.sakashita@gmail.com>
2021-11-17 13:57:24 +0900

    [add]'Resister Color Trio'の完成

--- ruby/resistor-color-trio/resistor_color_trio.rb
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -1,7 +1,40 @@
-=begin
-Write your code for the 'Resistor Color Trio' exercise in this file. Make the tests in
-`resistor_color_trio_test.rb` pass.
+class ResistorColorTrio
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-trio` directory.
-=end
+  COLOR_CODED_BANDS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+  COLORS = COLOR_CODED_BANDS.keys.map(&:to_s)
+  
+  def initialize(colors)
+    @colors = colors
+  end
+  
+  def color_code(colors)
+    raise ArgumentError unless colors.all?{|c| COLORS.include?(c)}
+    colors[0..1].map(&:to_sym).each.inject([]){|array, sym|
+      array << COLOR_CODED_BANDS[sym] }.join.to_i * 10 ** COLOR_CODED_BANDS[colors.last.to_sym]
+  end
+
+  def label
+    "Resistor value: #{Ohm.to_ohm(color_code(@colors))}"
+  end
+end
+
+
+class Ohm
+  class << self
+    def to_ohm(n)
+      n / 10 ** 3 >= 1 ? "#{n / 10 ** 3} kiloohms" : "#{n} ohms"
+    end
+  end
+end
