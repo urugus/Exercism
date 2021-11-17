c3f795593936367acb60bb04512fa7f71fea06d2
s-sakashi <suguru.sakashita@gmail.com>
2021-11-17 11:30:26 +0900

    [add]'Matrix'の完成

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,7 +1,13 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+class Matrix
+  def initialize(input)
+    @input = input
+  end
+  
+  def rows
+    @input.split("\n").map(&:split).map{|i| i.map(&:to_i)}
+  end
+  
+  def columns
+    rows.transpose
+  end
+end
