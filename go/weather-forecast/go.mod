fe4ed95695c78d0409c68a90ba3258358b3c3d9d
urugus <suguru.sakashita@gmail.com>
2025-05-18 22:16:46 +0900

    go: answer weather-forecast

--- go/weather-forecast/go.mod
+++ go/weather-forecast/go.mod
@@ -1,3 +1,11 @@
 module weather
 
-go 1.18
+go 1.23.0
+
+toolchain go1.24.3
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/tools v0.33.0 // indirect
+)
--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides weather-related functionalities.
 package weather
 
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is the current location for the weather forecast.
 var CurrentLocation string
 
+// Forecast returns text describing the current weather condition for a given city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
