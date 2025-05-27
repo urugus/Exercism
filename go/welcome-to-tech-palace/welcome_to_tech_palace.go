bd14c4acf02e1be6d237b93817a6b947ac4ed48e
urugus <suguru.sakashita@gmail.com>
2025-05-27 22:53:57 +0900

    go: answer welcome-to-tech-place

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,24 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	welcomeTemplate := "Welcome to the Tech Palace, "
+	upperCustomer := strings.ToUpper(customer)
+	return welcomeTemplate + upperCustomer
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	styling := strings.Repeat("*", numStarsPerLine)
+	return styling + "\n" + welcomeMsg + "\n" + styling
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	rawMsgBody := strings.Split(oldMsg, "\n")[1]
+	msgBody := strings.Trim(rawMsgBody, "*")
+	trimmedMsgs := strings.TrimSpace(msgBody)
+	return trimmedMsgs
 }
