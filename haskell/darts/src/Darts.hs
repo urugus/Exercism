d73a1fe1feff05242e6277ea88b9c67bf9ad3fb3
urugus <21075381+urugus@users.noreply.github.com>
2025-10-01 12:51:55 +0900

    haskell: complete `darts`

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -1,4 +1,10 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+score x y
+  | distance <= 1.0 = 10
+  | distance <= 5.0 = 5
+  | distance <= 10.0 = 1
+  | otherwise = 0
+  where
+    distance = sqrt (x ** 2 + y ** 2)
