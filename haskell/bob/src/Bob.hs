2ab0dc7401a16a539e33aeb3a385f5ce2db02aa2
urugus <21075381+urugus@users.noreply.github.com>
2025-10-04 22:05:27 +0900

    haskell: complete `bob`

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,14 @@
 module Bob (responseFor) where
 
+import Data.Char (isAlpha, isUpper)
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor input
+  | isClaim && isQuestion = "Calm down, I know what I'm doing!"
+  | isQuestion = "Sure."
+  | null input = "Fine. Be that way!"
+  | isClaim = "Whoa, chill out!"
+  | otherwise = "Whatever."
+  where
+    isQuestion = last input == '?'
+    isClaim = all isUpper (filter isAlpha input)
