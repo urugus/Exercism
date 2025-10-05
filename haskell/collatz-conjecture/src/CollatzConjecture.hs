9c904a2165aa60b05138842819d6ac8af3348749
urugus <21075381+urugus@users.noreply.github.com>
2025-10-05 11:36:33 +0900

    haskell: complete

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,8 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz n
+  | n <= 0 = Nothing
+  | n == 1 = Just 0
+  | even n = fmap (+ 1) (collatz (n `div` 2))
+  | otherwise = fmap (+ 1) (collatz (3 * n + 1))
