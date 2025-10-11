9eaa1235ee3b53712e6f557aa4527ec8a942aaa3
urugus <21075381+urugus@users.noreply.github.com>
2025-10-11 18:56:07 +0900

    haskell: complete `nucleotide count`

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,29 @@
-module DNA (nucleotideCounts, Nucleotide(..)) where
+module DNA (nucleotideCounts, Nucleotide (..)) where
 
 import Data.Map (Map)
+import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+charToNucleotide :: Char -> Maybe Nucleotide
+charToNucleotide 'A' = Just A
+charToNucleotide 'C' = Just C
+charToNucleotide 'G' = Just G
+charToNucleotide 'T' = Just T
+charToNucleotide _ = Nothing
+
+maybeToEither :: Maybe a -> Either String a
+maybeToEither (Just x) = Right x
+maybeToEither Nothing = Left "error"
+
+emptyCounts :: Map Nucleotide Int
+emptyCounts = Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+
+updateCount :: Map Nucleotide Int -> Nucleotide -> Map Nucleotide Int
+updateCount counts nucleotide = Map.insertWith (+) nucleotide 1 counts
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs =
+  case maybeToEither (traverse charToNucleotide xs) of
+    Left err -> Left err
+    Right counts -> Right (foldl updateCount emptyCounts counts)
