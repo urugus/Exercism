d3252924fa090b5c21adabd882cedaec0f655680
urugus <21075381+urugus@users.noreply.github.com>
2025-10-19 20:11:03 +0900

    haskell: complete `grains`

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,14 @@
 module Grains (square, total) where
 
+import Data.Maybe (mapMaybe)
+
+squareLimit :: Integer
+squareLimit = 64
+
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n
+  | n >= 1 && n <= squareLimit = Just (2 ^ (n - 1))
+  | otherwise = Nothing
 
 total :: Integer
-total = error "You need to implement this function."
+total = sum $ mapMaybe square [1 .. squareLimit]
